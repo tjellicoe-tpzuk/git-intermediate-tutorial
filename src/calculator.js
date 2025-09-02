ffff8cf731d4452a47aa047858ac78c21697a457
tjellicoe <thomas.jellicoe@telespazio.com>
2025-09-02 08:59:57 +0100

    This is a combination of 4 commits.
    comment
    
    correct multiply
    
    handle divide by zero
    
    Update logging

--- src/calculator.js
+++ src/calculator.js
@@ -1,10 +1,11 @@
 function divide(a, b) {
-
+  if (b == 0) throw new Error("Division by zero");
   return a / b;
 }
 
 function multiply(a, b) {
-  return (a + b + Math.random());
+  return (a * b);
 }
 
 module.exports = { divide, multiply };
+// comments
